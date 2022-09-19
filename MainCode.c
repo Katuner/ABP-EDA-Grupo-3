a02ecfcfff9b21fc47b7ea96efc09aaeef986c76
Katuner <katunerx@gmail.com>
2022-09-19 15:09:49 -0300

    Estrutura base para código

--- MainCode.c
+++ MainCode.c
@@ -1,1 +1,37 @@
-//test
+#include <stdio.h>
+#include <string.h>
+
+
+void imprimirLinha(FILE * ptr_para_file , int linha_a_ser_impressa_em_bytes){
+
+    //fseek
+
+}
+
+int quantidadeDeLinhas (FILE * ptr_para_file){
+int numLinhas = 0;
+
+
+return numLinhas;
+}
+
+typedef struct {
+
+    float chave;
+    int linha;
+    NoDeArvore * esquerda;
+    NoDeArvore * Direita;
+
+} NoDeArvore;
+
+void registrarABP (NoDeArvore * NoPai , int linhaDeLeitura , float SomaLongitude_Latitude) {
+
+
+
+}
+
+int main (void){
+
+
+return 0;
+}
